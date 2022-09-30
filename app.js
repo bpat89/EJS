cbcfb2501e650242009769dd563050bd567050ae
Patrick <bilshanpatrick@gmail.com>
2022-09-30 20:05:35 +0400

    runnning code inside the EJS Template

--- app.js
+++ app.js
@@ -2,46 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res) {
   var today = new Date();
   var currentDay = today.getDay();
-  // var nameofDay = today.toLocaleString('en-us', {weekday: 'long'});;
-  var day = "";
+  var options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+  var day = today.toLocaleString("en-US" , options);
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: Current day is equal to : " + currentDay);
-
-  }
   res.render('list', {
     kindofDay: day
   });
 });
-
+app.post("/" , function (req,res){
+  console.log(req.body.newItem);
+})
 app.listen(3000, function() {
   console.log("server is running on port 3000");
 })
