c253d67b114d172ea91ade9118dfa5b7986b1bf3
Patrick <bilshanpatrick@gmail.com>
2022-10-02 16:07:19 +0400

    starting to style the page

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+let items = ["Buy","Food", "Cook", "Eat",];
 
-app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res) {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var options = {
+  let today = new Date();
+  let currentDay = today.getDay();
+  let options = {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
     day: 'numeric'
   };
-  var day = today.toLocaleString("en-US" , options);
+  let day = today.toLocaleString("en-US", options);
 
   res.render('list', {
-    kindofDay: day
+    kindofDay: day,
+    newListItems: items
   });
 });
-app.post("/" , function (req,res){
-  console.log(req.body.newItem);
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+items.push(item);
+
+  res.redirect("/");
+
 })
 app.listen(3000, function() {
   console.log("server is running on port 3000");
